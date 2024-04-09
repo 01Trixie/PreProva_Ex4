0ab7d1141c1926da88ce4adb725e866930b4a8b2
Beatriz Oliveira <beafatec@gmail.com>
2024-04-09 16:03:40 -0300

    commit 2

--- src/controller/ClassController.java
+++ src/controller/ClassController.java
@@ -1,66 +1,55 @@
 
 package controller;
 
-import java.util.concurrent.Semaphore;
 
 public class ClassController extends Thread {
 	private int idThread;
 	private int N;
-	private Semaphore semaforoRec;
-	private Semaphore semaforoFunc;
 
-	public ClassController(int idThread, int N, Semaphore semaforoRec, Semaphore semaforoFunc) {
+
+	public ClassController(int idThread, int N) {
 		this.idThread = idThread;
 		this.N = N;
-		this.semaforoRec = semaforoRec;
-		this.semaforoFunc = semaforoFunc;
+		
 	}
 
 	public void run() {
+		
 		calculo();
 	}
 
 	private void calculo() {
-		int cta = 0;
-		int limite = 1;
-		boolean ocupado = false;
-		while (cta < limite) {
-			try {
-				sleep(1000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			if(cta == 0 && ocupado == false ) {
-				ocupado = true;
-			try {
-				semaforoRec.acquire();
+		int respRec=0;
+		int fat = 1;
+		int tempo = 1000; // a thread fica 1000 milissegundos bloqueada
+		
+		try {
+			sleep(tempo);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		switch(idThread) {
+			case 0:
 				long startRec = System.nanoTime();
+				respRec = recFat(N);
 				long endtRec = System.nanoTime();
-				long totalRec = System.nanoTime();
-				totalRec = endtRec - startRec;
+				long totalRec = endtRec - startRec;
 				System.out.println("A thread recursiva #" + idThread + " demorou " + totalRec 
-						+ " nanossegundos para calcular o fat de " + N + " que foi: " + recFat(N));
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				semaforoRec.release();
-			}
-			} 
-			
-			try {
-				semaforoFunc.acquire();
-				funcFat(N);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} finally {
-				semaforoFunc.release();
-
-			}
-			 
-			cta++;
+						+ " nanossegundos para calcular o fat de " + N + " que foi: " + respRec);
+				break;
+			case 1: 
+				long startFunc = System.nanoTime();
+				fat = funcFat(N);
+				long endFunc = System.nanoTime();
+				long totalFunc = endFunc - startFunc;
+				System.out.println("A Thread função  #" + idThread + "  demorou " + totalFunc
+						+ " nanossegundos para calcular Fat de " + N + " que é: " + fat);
+				break;
+				
 		}
 	}
-	
+			
 	private static int recFat(int N) {
 
 		if (N > 0) {
@@ -70,15 +59,12 @@ public class ClassController extends Thread {
 
 		}
 
-	private void funcFat(int N) {
+	private static int funcFat(int N) {
 		int fat = 1;
-		long startFunc = System.nanoTime();
+		
 		for (int i = 0; i < N; i++) {
 			fat +=  i * fat;
 		}
-		long endFunc = System.nanoTime();
-		long totalFunc = endFunc - startFunc;
-		System.out.println("A Thread função  #" + idThread + "  demorou " + totalFunc
-				+ " nanossegundos para calcular Fat de " + N + " que é: " + fat);
+		return fat;
 	}
 }
